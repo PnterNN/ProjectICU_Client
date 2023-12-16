7417f7730e9ec9f8e8ccbe7cc61411b0f2fb724c
pnternn <kubilaybaltaoglu2004@outlook.com.tr>
2023-12-16 14:01:32 +0300

    login iletişimleri yapılıyor

--- Net/Server.cs
+++ Net/Server.cs
@@ -5,24 +5,46 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ProjectICU_Client.Net
 {
     internal class Server
     {
         private static TcpClient _client;
+        public PacketReader PacketReader;
         public Server()
         {
             _client = new TcpClient();
             _client.Connect("127.0.0.1", 9001);//46.31.77.173
 
+            PacketReader = new PacketReader(_client.GetStream());
+
             Task.Delay(3000);
 
-            var LoginPacket = new PacketBuilder();
+            PacketBuilder LoginPacket = new PacketBuilder();
             LoginPacket.WriteOpCode(0);
-            LoginPacket.WriteMessage("Deneme1");
-            LoginPacket.WriteMessage("Deneme2");
+            LoginPacket.WriteMessage("pnternn@hotmail.com");
+            LoginPacket.WriteMessage("asd123Hh");
             _client.Client.Send(LoginPacket.GetPacketBytes());
+            ReadPackets();
+
+        }
+
+        public void ReadPackets()
+        {
+            Task.Run(() => 
+            { 
+                var opcode = PacketReader.ReadByte();
+                switch (opcode)
+                {
+                    case 0:
+                        string username = PacketReader.ReadString();
+                        string uid = PacketReader.ReadString();
+                        MessageBox.Show(username + " " + uid);
+                        break;
+                }
+            });
         }
     }
 }
