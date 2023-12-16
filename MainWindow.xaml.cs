8c45c71da37892fe277406af47b0171051aba37c
pnternn <kubilaybaltaoglu2004@outlook.com.tr>
2023-12-16 13:27:55 +0300

    Proje dosyası ekle.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ProjectICU_Client.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectICU_Client
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Delay(1000);
+            Server server = new Server();
+        }
+    }
+}
---
+++ Net/Server.cs
@@ -0,0 +1,28 @@
+﻿using ProjectICU_Server.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectICU_Client.Net
+{
+    internal class Server
+    {
+        private static TcpClient _client;
+        public Server()
+        {
+            _client = new TcpClient();
+            _client.Connect("127.0.0.1", 9001);//46.31.77.173
+
+            Task.Delay(3000);
+
+            var LoginPacket = new PacketBuilder();
+            LoginPacket.WriteOpCode(0);
+            LoginPacket.WriteMessage("Deneme1");
+            LoginPacket.WriteMessage("Deneme2");
+            _client.Client.Send(LoginPacket.GetPacketBytes());
+        }
+    }
+}
